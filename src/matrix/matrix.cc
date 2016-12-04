dd8341d00342df54034b2ce450f150ca1ef7c2e2
Alexis Ford <ford128@mail.chapman.edu>
2016-12-03 22:29:16 -0800

    added functions

--- src/matrix/matrix.cc
+++ src/matrix/matrix.cc
@@ -3,6 +3,10 @@
 #define CW13_MATRIX_CPP_
 
 // include the header declarations
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <fstream>
 #include "matrix.h"
 
 //*************
@@ -132,5 +136,121 @@ Matrix<T> Matrix<T>::operator+(const Matrix<T>& rhs) {
   
     return result;
 }
+
+//Matrix multiplication
+template<typename T>
+Matrix<T> Matrix<T>::operator*(const Matrix<T>& rhs)
+{
+    if(this->cols != rhs.get_rows())
+    {
+        //can only multiply if cols of first equal to rows of second
+        throw "Invalid matrix dimensions.";
+    }    
+    Matrix result(this->rows, rhs.get_cols(), 0.0);
+    //keep track of sum of products for each new element of results
+    T elemProdSum = 0;
+    //i and j depend on dimensions of result matrix
+    for (unsigned int i=0; i<result.get_rows(); i++) 
+    {
+        for (unsigned int j=0; j<result.get_cols(); j++) 
+        {
+            //k depends on cols of first = rows of second
+            for (unsigned int k=0; k<(this->cols); k++)
+            {
+                //each row of first with each column of second
+                elemProdSum += (this->mat[i][k])*rhs(k,j);
+            }
+            result(i,j) = elemProdSum;
+            //reset sum
+            elemProdSum = 0;
+        }
+    }
+    return result;   
+}
+template<typename T>
+Matrix<T> Matrix<T>::operator+(const T& scalar)
+{
+    Matrix result(this->rows, this->cols, 0.0);
+    for (unsigned int i=0; i<this->rows; i++) 
+    {
+        for (unsigned int j=0; j<this->cols; j++) 
+        {
+            result(i,j) = this->mat[i][j] + scalar;
+        }
+    }
+    return result;
+}
+
+//**********************
+//
+//  Additional Methods
+//
+//**********************
+
+
+//Print method
+template<typename T>
+void Matrix<T>::print() const
+{
+    for (unsigned int i=0; i < rows; i++)
+    {
+        for (unsigned int j=0; j < cols; j++)
+        {
+            std::cout << this->mat[i][j] << "\t";
+        }
+        std::cout << std::endl;
+    }
+}
+
+//Transpose method
+template<typename T>
+Matrix<T> Matrix<T>::transpose() const
+{
+    Matrix tMat(this->cols, this->rows, 0.0);
+    for (unsigned int i=0; i<this->rows; i++) 
+    {
+        for (unsigned int j=0; j<this->cols; j++) 
+        {
+            tMat(j,i) = this->mat[i][j];
+        }
+    }
+
+    return tMat;
+}
+
+//Save matrix to file
+template<typename T>
+void Matrix<T>::save(std::string filename) const
+{
+    std::ofstream output_file;
+
+    output_file.open(filename.c_str());
+
+    // check that file opened successfully
+    if ( !output_file.is_open() )
+    {
+        std::cerr << "Error: Could not open file" << std::endl;
+    }
+    else
+    {
+        for (unsigned int i=0; i < this->rows; i++)
+        {
+            for (unsigned int j=0; j < this->cols; j++)
+            {
+                //if-else statement to prevent "," after last element of row
+                if (j != (this->cols) - 1)
+                    output_file << this->mat[i][j] << ",";
+                //if-else also works to add newline
+                else
+                    output_file << this->mat[i][j] << "\n";
+            }
+        }
+        output_file.close();
+    }
+}
+
+
+
+
 
 #endif // CW13_MATRIX_CPP_
